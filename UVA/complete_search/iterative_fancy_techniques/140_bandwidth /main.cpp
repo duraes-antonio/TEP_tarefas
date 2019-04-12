b422d245751f6544707064286a272038d96fea82
duraes-antonio <garotoseis@gmail.com>
2019-04-12 06:35:47 -0300

    CLEAN: Limpeza dos arquivos do exercício 140

--- UVA/complete_search/iterative_fancy_techniques/140_bandwidth /main.cpp
+++ UVA/complete_search/iterative_fancy_techniques/140_bandwidth /main.cpp
@@ -1,134 +1,120 @@
-//#include <iostream>
-//#include <map>
-//#include <vector>
-//#include <algorithm>
-//
-//#define MAP_CHAR_CHAR map<char, vector<char>>
-//
-//using namespace std;
-//
-//MAP_CHAR_CHAR tratar_entrada(string entrada) {
-//
-//    MAP_CHAR_CHAR saida;
-//    vector<char> temp_viz;
-//    char ult_chave;
-//    bool dois_pt = false;
-//
-//    for (char caract : entrada) {
-//
-//        if(caract == ':') dois_pt = true;
-//
-//            /*Se o char. for ';', crie a chave com a lista de vizinhos lidos;*/
-//        else if(caract == ';') {
-//            dois_pt = false;
-//            saida[ult_chave] = temp_viz;
-//            temp_viz.clear();
-//        }
-//
-//            /*Salve a letra antes dos dois pontos*/
-//        else if(not dois_pt) ult_chave = caract;
-//
-//            /*Se já leu ':', o caracter atual é um dos nós vizinhos, adicione na lista;
-//             *O char. chave é vizinho do char atual também*/
-//        else {
-//            temp_viz.push_back(caract);
-//
-//            if(find(saida[caract].begin(), saida[caract].end(), ult_chave) == saida[caract].end()) {
-//                saida[caract].push_back(ult_chave);
-//            }
-//        }
-//    }
-//
-//    saida[ult_chave] = temp_viz;
-//
-//    return saida;
-//}
-//
-//void imprimir_entrada(string entrada) {
-//
-//    MAP_CHAR_CHAR result = tratar_entrada(entrada);
-//
-//    for(auto par: result) {
-//        cout << par.first << ": ";
-//
-//        for (char i : par.second) {
-//            cout << i << " ";
-//        }
-//
-//        cout << endl;
-//    }
-//}
-//
-//int busca_rec(MAP_CHAR_CHAR map, vector<char> cand, vector<char> black, char busc) {
-//
-//    int dist, menor_dist;
-//    dist = menor_dist = 0;
-//
-//    if(cand.empty()) return 0;
-//
-//    /*Se o nó buscado for vizinho do atual analizado*/
-//    if(find(cand.begin(), cand.end(), busc) != cand.end()) menor_dist = 1;
-//
-//    /*Senão, adicione os vizinhos do atual p/ serem analizados e incr. a dist.*/
-//    else {
-//
-//        for (char vizinho: cand) {
-//            black.push_back(cand[0]);
-//            cand.erase(cand.begin() + 0);
-//
-//            for (char atual_viz: map[vizinho]) {
-//
-//                if (find(cand.begin(), cand.end(), atual_viz) == cand.end() &&
-//                    find(black.begin(), black.end(), atual_viz) == cand.end()) {
-//                    cand.push_back(atual_viz);
-//                }
-//            }
-//
-//            dist = 1 + busca_rec(map, cand, black, busc);
-//
-//            if(dist < menor_dist || !menor_dist) menor_dist = dist;
-//        }
-//    }
-//
-//    return menor_dist;
-//}
-//
-//int calc_menor_dist(MAP_CHAR_CHAR map, char no1, char no2) {
-//
-//    int menor_dist, dist;
-//
-//    if(find(map[no1].begin(), map[no1].end(), no2) != map[no1].end()) return 1;
-//
-//    vector<char> candidatos;
-//    candidatos = map[no1];
-//
-//    while(!candidatos.empty()) {
-//
-//    }
-//
-//    return 1;
-//}
-//
-//int main() {
-//
-//    int max_custo;
-//    string entrada;
-//
-//    while(cin >> entrada and entrada != "#") {
-//        map<char, vector<char>> result = tratar_entrada(entrada);
-//
-//        for (auto par: result) {
-//            vector<char> lista = {par.first};
-//
-//            for (auto dest: result) {
-//                if(par.first == dest.first) continue;
-//
-//                printf("%c -> %c - D = %d\n", par.first, dest.first,
-//                        busca_rec(result, result[par.first], lista, dest.first));
-//            }
-//
-//            cout << endl;
-//        }
-//    };
-//
-//}
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+#define MAP_CHAR_CHAR map<char, vector<char>>
+
+using namespace std;
+
+MAP_CHAR_CHAR tratar_entrada(string entrada) {
+
+    MAP_CHAR_CHAR saida;
+    vector<char> temp_viz;
+    char ult_chave;
+    bool dois_pt = false;
+
+    for (char caract : entrada) {
+
+        if(caract == ':') dois_pt = true;
+
+            /*Se o char. for ';', crie a chave com a lista de vizinhos lidos;*/
+        else if(caract == ';') {
+            dois_pt = false;
+            saida[ult_chave] = temp_viz;
+            temp_viz.clear();
+        }
+
+            /*Salve a letra antes dos dois pontos*/
+        else if(not dois_pt) ult_chave = caract;
+
+            /*Se já leu ':', o caracter atual é um dos nós vizinhos, adicione na lista;
+             *O char. chave é vizinho do char atual também*/
+        else {
+            temp_viz.push_back(caract);
+
+            if(find(saida[caract].begin(), saida[caract].end(), ult_chave) == saida[caract].end()) {
+                saida[caract].push_back(ult_chave);
+
+                if(find(saida[ult_chave].begin(), saida[ult_chave].end(), caract) == saida[ult_chave].end()) {
+                    saida[ult_chave].push_back(caract);
+                }
+            }
+        }
+    }
+
+    return saida;
+}
+
+void imprimir_entrada(string entrada) {
+
+    MAP_CHAR_CHAR result = tratar_entrada(entrada);
+
+    for(auto par: result) {
+        cout << par.first << ": ";
+
+        for (char i : par.second) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+bool in(vector<char> vetor, char buscado) {
+    return find(vetor.begin(), vetor.end(), buscado) != vetor.end();
+}
+
+void obter_caminhos(MAP_CHAR_CHAR grafo, vector<char> black, char inic, char obj, string caminho, vector<string>* saida) {
+
+    caminho += inic;
+
+    /*Se o nó já estiver na lista de visitados, saia da função*/
+    if(in(black, inic)) return;
+
+    /*Adicione o nó na lista de visitados;*/
+    black.push_back(inic);
+
+    /*Se encontrou o nó buscado;
+     *Adicione o caminho ao conjunto de caminhos possíveis;
+     *Reinicie o texto do caminho*/
+    if(inic == obj) {
+        saida -> push_back(caminho + "\n");
+        caminho.clear();
+    }
+
+    /*Senão, repita o processo para cada vizinho do nó corrente*/
+    else for(char &i : grafo[inic]) {
+        obter_caminhos(grafo, black, i, obj, caminho, saida);
+    }
+}
+
+int main() {
+
+    int max_custo;
+    string entrada;
+    vector<string> saida;
+    vector<char> chars_in;
+    MAP_CHAR_CHAR mapa;
+
+    /*Leia a entrada enquanto ela for diferente de '#'*/
+    while(cin >> entrada and entrada != "#") {
+        mapa = tratar_entrada(entrada);
+
+        for(auto par: mapa) chars_in.push_back(par.first);
+
+        /*Para cada nó do map*/
+        for (int i = 0; i < mapa.size(); ++i) {
+
+            /*Obtenha todos possíveis caminhos até o nó adjacente*/
+            for (int j = i+1; j < mapa.size(); ++j) {
+
+                /*Obtenha o conjunto com os caminhos possíveis*/
+                obter_caminhos(mapa, {}, chars_in[i], chars_in[j], "", &saida);
+
+                for(const string &str: saida) cout << str;
+            }
+        }
+    };
+}
