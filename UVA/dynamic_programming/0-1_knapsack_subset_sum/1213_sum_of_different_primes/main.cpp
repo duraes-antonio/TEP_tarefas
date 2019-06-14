27286233b812b2576d15891b29bd855404912ead
duraes-antonio <garotoseis@gmail.com>
2019-06-14 12:01:03 -0300

    NEW: Resolução #1213 + Passo a passo + ideia de como resolver

--- UVA/dynamic_programming/0-1_knapsack_subset_sum/1213_sum_of_different_primes/main.cpp
+++ UVA/dynamic_programming/0-1_knapsack_subset_sum/1213_sum_of_different_primes/main.cpp
@@ -1,49 +1,77 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
 
 #define MAX_NUM 1121
+#define MAX_N   15
 
 using namespace std;
 
-int primos[MAX_NUM];
-vector<int> primos_somente;
+vector<int> g_primos_somente;
+int g_num_n[MAX_NUM][MAX_N];
 
+/*Verifica se o inteiro de entrada é um número primo*/
 bool primo(int num) {
 
-    if(num < 2) return false;
-    if(num % 2 == 0 && num != 2) return false;
+    if (num < 2) return false;
+    if (num % 2 == 0 && num != 2) return false;
 
     double raiz = sqrt(num);
 
     for (int i = 3; i <= raiz; i += 2) {
-        if(num % i == 0) return false;
+        if (num % i == 0) return false;
     }
 
     return true;
 }
 
+/*Preenche o vetor global com a quant. suficiente de primos p/ resolver o problema */
 void gerar_primos() {
 
     for (int i = 0; i <= MAX_NUM; ++i) {
-        primos[i] = primo(i);
 
-        if(primos[i]) primos_somente.push_back(i);
+        if (primo(i)) g_primos_somente.push_back(i);
+    }
+}
+
+void gerar_resposta() {
+
+    /*Inicialize toda as células da matriz com 0*/
+    memset(g_num_n, 0, MAX_NUM*MAX_N);
+
+    /*Caso base (CB): importante, pois ao subtrair um primo de si próprio, a
+     * o número de combinações dependerá do resultado do caso inicial*/
+    g_num_n[0][0] = 1;
+
+    /*Para cada número primo no vetor que contém apenas primos*/
+    for (int primo: g_primos_somente) {
+
+        /*Para cada número entre máximo permitido e o primo atual*/
+        for (int num = MAX_NUM-1; num > 0; --num) {
+
+            if(num < primo) break;
+
+            for (int n = 1; n < MAX_N; ++n) {
+                g_num_n[num][n] += g_num_n[num - primo][n-1];
+            }
+        }
     }
 }
 
 int main() {
 
-    int num_input, n_formas;
+    int num_input, n_primos;
+
     gerar_primos();
+    gerar_resposta();
 
-    while(cin >> num_input && num_input > 0) {
+    while (cin >> num_input) {
 
-        cin >> n_formas;
+        cin >> n_primos;
+        if (num_input < 1 && n_primos < 1) break;
 
-        for (int i = 0; i < 14; ++i) {
-            
-        }
+        cout << (g_num_n[num_input])[n_primos] << endl;
     }
 
     return 0;
